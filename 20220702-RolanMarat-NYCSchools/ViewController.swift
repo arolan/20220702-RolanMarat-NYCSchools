a258f704373b00c3152acb7cb54049950d4f1db9
Rolan Marat <arolan@gmail.com>
2022-07-02 20:38:28 -0700

    Initial Commit

---
+++ 20220702-RolanMarat-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20220702-RolanMarat-NYCSchools
+//
+//  Created by Rolan on 7/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
